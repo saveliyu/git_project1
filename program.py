080fd91b3dba7ed30b2cfce2b4df1c102feeac11
saveliyu <128820673+saveliyu@users.noreply.github.com>
2023-11-28 22:03:09 +0700

    nifigasebe

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('my password ya eblan 1337')
+    print('sasat lohi')
 
 
 if __name__ == '__main__':
